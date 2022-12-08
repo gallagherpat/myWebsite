946790bf345d203fb018447eff9095f5d0677f75
gallagherpat <gallagherpatrick21@gmail.com>
2022-12-07 20:37:22 -0500

    fixed a bunch of stuff and broke some things

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-if (module.hot) {
-    module.hot.accept();
-}
+// if (module.hot) {
+//     module.hot.accept();
+// }
 
 
 let menuOpen = false;
@@ -32,3 +32,68 @@ const firebaseConfig = {
     measurementId: "G-K6D7T7N18B"
 };
 
+let body = document.querySelector('body');
+
+
+
+const navigationHandler = function (id) {
+    id = Number(this.id);
+    switch (id) {
+        case 0:
+            window.location.href = 'test.html';
+            break;
+        case 1:
+            window.location.href = 'about.html';
+            break;
+        case 2:
+            window.location.href = 'logs.html'
+            break;
+        case 3:
+            window.location.href = 'contact.html'
+            break;
+        default:
+            console.log('nav handler broke')
+    }
+    console.log(id);
+}
+
+
+const navLoader = function () {
+    let name;
+    let id;
+    let html;
+    let navContainer = document.querySelector('.nav');
+
+    for (let i = 0; i < 4; i++) {
+        switch (i) {
+            case 0:
+                name = 'Home';
+                html = `<button id="${id = i}">${name}</button>`;
+                navContainer.insertAdjacentHTML('beforeend', html);
+                break;
+            case 1:
+                name = 'About';
+                html = `<button id="${id = i}">${name}</button>`;
+                navContainer.insertAdjacentHTML('beforeend', html);
+                break;
+            case 2:
+                name = 'Logs';
+                html = `<button id="${id = i}">${name}</button>`;
+                navContainer.insertAdjacentHTML('beforeend', html);
+                break;
+            case 3:
+                name = 'Contact';
+                html = `<button id="${id = i}">${name}</button>`;
+                navContainer.insertAdjacentHTML('beforeend', html);
+                break;
+            default:
+                console.log(`switch broke at ${i}`);
+        }
+        let navBtn = document.querySelectorAll('button');
+        navBtn[i].addEventListener('click', navigationHandler);
+    }
+
+}
+
+
+body.addEventListener('load', navLoader());
